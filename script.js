c43afdc620068ecd71ea1a51c3ddb0e8098703e5
ABDULLAH JUTT <111068193+ABDULLAH8543@users.noreply.github.com>
2023-09-16 04:17:14 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+let contact = document.getElementById("contact");
+let btn = document.getElementById("btn");
+
+contact.addEventListener("click", function () {
+        window.location.assign("contact.html");
+});
+btn.addEventListener("click", function () {
+        window.location.assign("contact.html");
+});
+let checkbtn = document.getElementById("check");
+let Languages = document.getElementById("Languages");
+
+Languages.addEventListener("click", function () {
+    let ul = document.querySelector("ul");
+    ul.style.left = "-100%";
+    checkbtn.checked = false;
+});
+let ul = document.querySelector("ul");
+let iElement = document.querySelector(".checkbtn i");
+let navbtn = document.getElementById("bnav");
+
+  checkbtn.addEventListener("click", function () {
+    let check = document.getElementById("check");
+
+    if (check.checked) {
+      ul.style.left = "0%";
+      iElement.textContent = "✖";
+    } else {
+      ul.style.left = "-100%";
+      iElement.textContent = "☰";
+    }
+  });
