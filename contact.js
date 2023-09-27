49cb376ec3a7a1e1886aa83bb003d0a37aa7c7b2
ABDULLAH JUTT <111068193+ABDULLAH8543@users.noreply.github.com>
2023-09-27 14:26:52 +0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,17 +1,4 @@
-// console.log("ok");
 let home = document.getElementById("home");
-// let fiverr = document.getElementById("fiverr");
-// let upwork = document.getElementById("up");
-// let link = document.getElementById("link");
 home.addEventListener("click", function() {
 window.location.assign("index.html");
 });
-// fiverr.addEventListener("click", function() {
-//           window.location.assign("https://www.fiverr.com/abdullahjutt561");
-//           });
-// upwork.addEventListener("click", function() {
-//           window.location.assign("https://www.upwork.com/nx/project-dashboard/?ref=fl_nav");
-//           });
-// link.addEventListener("click", function() {
-//           window.location.assign("https://www.linkedin.com/in/abdullah-jutt-56aa90262/");
-//           });
