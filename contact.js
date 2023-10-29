790e32572807cac7d1251cfaa37ac6d541c905a8
ABDULLAH JUTT <111068193+ABDULLAH8543@users.noreply.github.com>
2023-10-29 21:47:00 +0500

    Update contact.js

--- contact.js
+++ contact.js
@@ -1,4 +1,8 @@
 let home = document.getElementById("home");
+let git = document.getElementById("git");
 home.addEventListener("click", function() {
 window.location.assign("index.html");
+});
+git.addEventListener("click", function() {
+window.location.assign("https://github.com/ABDULLAH8543");
 });
